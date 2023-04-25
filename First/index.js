adcb5c242fe8a0f5e0aaa11de7534fda4b2b3031
Tachi Dito <tachidito_7inc@hotmail.co>
2023-04-24 19:05:07 -0600

    fin del curso

---
+++ First/index.js
@@ -0,0 +1,67 @@
+// alert("Esto si es un lenguaje de programación");
+
+// alert('La edad de Tachi es...');
+// alert('!?')
+let anio = 2022
+let edadAx = 4;
+let edadMar = 2;
+let edadTachi = 39;
+// let saltoLinea = '<br>'
+
+function jumpLine(){
+    document.write('<br>');
+}
+
+function print(text){
+    document.write(text);
+    jumpLine()
+}
+
+print('Hola Cachorros')
+
+print('El promedio de edad de Ax, Mar y Tachi es...');
+document.write((edadAx+edadMar+edadTachi)/3);
+
+print('La suma de las edades de Ax, May y Tachi es...'+ (edadAx+edadMar+edadTachi));
+
+print('Ax tiene ' + (anio-2018) + ' años');
+
+print('Mar tiene '+ (anio-2020) +' años');
+
+anio = 2030
+
+print('Tachi tendra ' +(anio-1983)+ ' años, en el año ' + anio );
+jumpLine()
+
+// let name1 = 'Ax'
+// let name2 = 'Mar'
+// let name3 = 'Tachi'
+
+// let promedio = (edadAx+edadMar+edadTachi)/3
+
+// print('Elpromedio de las edades de ' +name1+ ', ' + name2 +' y '+name3+' es de: '+ Math.round(promedio))
+
+// let multiplo = 5
+// print('<h1>Tabla del 5</h1>');
+// print("5 por 1 es " + multiplo * 1 + "<br>");
+// print("5 por 2 es " + multiplo * 2 + "<br>");
+// print("5 por 3 es " + multiplo * 3 + "<br>");
+// print("5 por 4 es " + multiplo * 4 + "<br>");
+// print("5 por 5 es " + multiplo * 5 + "<br>");
+// print("5 por 6 es " + multiplo * 6 + "<br>");
+// print("5 por 7 es " + multiplo * 7 + "<br>");
+// print("5 por 8 es " + multiplo * 8 + "<br>");
+// print("5 por 9 es " + multiplo * 9 + "<br>");
+// print("5 por 10 es " + multiplo * 10 + "<br>");
+// multiplo = 8
+// print('<h1>Tabla del 8</h1>');
+// print("5 por 1 es " + multiplo * 1 + "<br>");
+// print("5 por 2 es " + multiplo * 2 + "<br>");
+// print("5 por 3 es " + multiplo * 3 + "<br>");
+// print("5 por 4 es " + multiplo * 4 + "<br>");
+// print("5 por 5 es " + multiplo * 5 + "<br>");
+// print("5 por 6 es " + multiplo * 6 + "<br>");
+// print("5 por 7 es " + multiplo * 7 + "<br>");
+// print("5 por 8 es " + multiplo * 8 + "<br>");
+// print("5 por 9 es " + multiplo * 9 + "<br>");
+// print("5 por 10 es " + multiplo * 10 + "<br>");
